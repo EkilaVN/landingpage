c4ca462ac5f0338054b4c4c6cbbd66d90f469eee
tduykhuong <tduykhuong.ekila@gmail.com>
2025-07-31 23:52:45 +0700

    feat: add GA

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,17 +1,15 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
 import { pageview } from "@/lib/gtag";
+import { usePathname } from "next/navigation";
 
 export default function Analytics() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
-    const url = pathname + searchParams.toString();
-    pageview(url);
-  }, [pathname, searchParams]);
+    pageview(pathname);
+  }, [pathname]);
 
   return null;
 }

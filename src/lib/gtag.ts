1463ccbe415afe6b417e0a8db0d8908b4c76797f
tduykhuong <tduykhuong.ekila@gmail.com>
2025-07-31 23:37:23 +0700

    feat: add GA

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,7 +1,17 @@
 export const GA_TRACKING_ID = "G-3HSXW3LWQ0";
 
+declare global {
+  interface Window {
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
 export const pageview = (url: string) => {
-  if (typeof window !== "undefined" && process.env.NODE_ENV === "production") {
+  if (
+    typeof window !== "undefined" &&
+    typeof window.gtag === "function" &&
+    process.env.NODE_ENV === "production"
+  ) {
     window.gtag("config", GA_TRACKING_ID, {
       page_path: url,
     });

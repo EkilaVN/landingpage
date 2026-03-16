2b33f61c78e4244aa95cc73ea708f821ac5ba98d
tduykhuong <tduykhuong.ekila@gmail.com>
2026-03-16 23:15:55 +0700

    fix: update cms repo

---
+++ cms/src/api/loads/controllers/loads.ts
@@ -0,0 +1,49 @@
+/**
+ * A set of functions called "actions" for `loads`
+ */
+import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3"
+
+export default {
+  async list(ctx) {
+    try {
+      const { app_id } = ctx.query;
+
+      const s3 = new S3Client({
+        region: process.env.AWS_REGION || "auto",
+        endpoint: process.env.AWS_ENDPOINT,
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_ACCESS_SECRET,
+        },
+        forcePathStyle: true,
+      })
+
+      const command = new ListObjectsV2Command({
+        Bucket: process.env.AWS_BUCKET,
+        ...(app_id ? { Prefix: app_id } : { }),
+      })
+
+      const response = await s3.send(command)
+
+      ctx.body = (response.Contents || [])
+        .map((obj) => ({
+          key: obj.Key,
+          size: obj.Size,
+          lastModified: obj.LastModified,
+          url: `${process.env.CUSTOM_DOMAIN}/${obj.Key}`,
+        }))
+        .filter((obj) => app_id ? true : (!obj.key.includes("/") || obj.key.startsWith("assets/")))
+        .filter((obj) => ((obj.key.endsWith(".css") || obj.key.endsWith(".js"))))
+        .reduce(
+          (acc, obj) => {
+            if (obj.key.endsWith(".css")) acc.css.push(obj.url)
+            else if (obj.key.endsWith(".js")) acc.js.push(obj.url)
+            return acc
+          },
+          { css: [], js: [] }
+        )
+    } catch (err) {
+      ctx.throw(500, err.message)
+    }
+  },
+}
---
+++ cms/src/api/project-industry/controllers/project-industry.ts
@@ -0,0 +1,7 @@
+/**
+ * project-industry controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::project-industry.project-industry');

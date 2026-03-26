5b6bb01c60a9de90c723fbdbc4ad77ddc77ef176
nducminh-ekila <nducminh.ekila@gmail.com>
2026-03-26 11:34:59 +0700

    integrate s3 upload

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -1,7 +1,23 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://market-assets.strapi.io",
+            "https://static.ekila.vn",
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
